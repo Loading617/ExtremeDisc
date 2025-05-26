0b1ee80abdee4adc2eec033d54f833bb3e149dd1
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-05-26 18:59:45 -0400

    Delete main.cpp

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <gtkmm.h>
-#include "ExtremeDiscWindow.hpp"
-
-int main(int argc, char *argv[]) {
-    auto app = Gtk::Application::create(argc, argv, "org.example.discburner");
-    ExtremeDiscWindow window;
-    return app->run(window);
-}
